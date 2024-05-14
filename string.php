5aa4ae1d398d4ebed306abb54276f88c95f7a63b
Anik Hassan1 <ah491606@gmail.com>
2024-05-15 00:41:34 +0600

    string Complate

---
+++ string.php
@@ -0,0 +1,18 @@
+<?php
+
+//__useing Array__//
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+//__Create a countVowelsRegex Function__//
+function countVowelsRegex($str) {
+     preg_match_all('/[aeiouAEIOU]/', $str, $matches);
+     return count($matches[0]);
+}
+//__foreach Loop__//
+foreach($strings as $string){
+    $orgStr=$string ;
+    $vowels=countVowelsRegex($string);
+    $rev= strrev($string);
+    echo "Original String: " . $orgStr.",  Vowel Count: ".$vowels.",  Reversed String: ".$rev."<br>";
+}
+?>
